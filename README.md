567d4ebf70cbd4e4f999e6df701ff5699890a3d7
Javier Hernández <javi@raisingthefloor.org>
2019-03-11 20:31:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ShareX-Portable installer merge module for Morphic
+
+This produces `sharex.msm`, which is used by gpii-wix-installer to bundle ShareX-Portable with Morphic. This will make the
+Morphic installer to install ShareX-Portable.
+
+## Usage
+
+* Run [build.ps1](build.ps1)
+* Take `output/filebeat.msm`.
+
---
+++ build.ps1
@@ -0,0 +1,27 @@
+<#
+  This script creates the wix merge module for gpii-wix-installer.
+#>
+
+Push-Location (Split-Path -parent $PSCommandPath)
+
+function Clean {
+    rm temp\*.wixobj, output\*.wixpdb
+}
+
+Clean
+
+## create a fragment (.wxs file) with the "sharex" folder that contains sharex-portable
+mkdir temp
+heat dir sharex -dr ShareXDirectory -ke -srd -cg ShareXDirectory -gg -var var.buildFolder -out temp\ShareXDirectory.wxs
+
+Remove-Item -Recurse output
+mkdir output
+
+## create the .wixobj files
+candle sharex.wxs temp\ShareXDirectory.wxs -dbuildFolder=sharex -out temp\
+
+## build the merge module
+light temp\sharex.wixobj temp\ShareXDirectory.wixobj  -sacl -o output/sharex.msm
+
+Clean
+Pop-Location
